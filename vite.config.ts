e1853c53a58f23b20630b8b5292c8abab565801f
Johann Goncalves Pereira <git.qjuid@passfwd.com>
2025-10-03 17:58:57 -0300

    Add Tailwind CSS and enhance React Scan configuration
    - Integrated Tailwind CSS plugin for styling support.
    - Updated React Scan plugin to enable tracking of unnecessary re-renders and auto display names for components.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import reactScan from '@react-scan/vite-plugin-react-scan'
+import reactScan from "@react-scan/vite-plugin-react-scan";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+    reactScan({
+      autoDisplayNames: true,
+      scanOptions: {
+        trackUnnecessaryRenders: true, // Enable tracking of unnecessary re-renders in React components
       },
     }),
   ],
